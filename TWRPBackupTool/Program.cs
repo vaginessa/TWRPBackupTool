39b33ede126c504a36eed25689ec7c56c213d738
Wisdom Jere <wisdomjere5@gmail.com>
2022-02-26 05:46:58 +0200

    refactor

--- TWRPBackupTool/Program.cs
+++ TWRPBackupTool/Program.cs
@@ -8,22 +8,7 @@ public class Program
     static void DisplayMenu ()
     {
         WriteLine("\t[1] Reboot to recovery\n\t[2] Backup\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
-    }
-    static Task RebootToRecovery()
-    {
-        var command = "/K adb reboot recovery";
-        var startInfo = new ProcessStartInfo
-        {
-            FileName = "cmd.exe",
-            Arguments = command,
-            UseShellExecute = true,
-            CreateNoWindow = true,
-            WindowStyle = ProcessWindowStyle.Hidden
-        };
-        Process.Start(startInfo);
-        WriteLine("Done.");
-        return Task.CompletedTask;
-    }
+    } 
    
     public static async Task Main()
     {
@@ -39,9 +24,21 @@ public class Program
            switch(choice)
            {
                 case 1:
-                    await RebootToRecovery();
-                    Write("Next Command: ");
-                    choice = ToInt32(ReadLine());
+                    WriteLine("=====================================================================");
+                    WriteLine("List of Devices attached");
+                    foreach (var device in devices)
+                    {
+                        WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
+                    }
+                    WriteLine("_____________________________________________________________________\n");
+                    Write("Choose Device: ");
+                    var exist = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(ReadLine()));
+                    if (exist == null)
+                    {
+                        WriteLine("Invalid entry.");
+                    }
+                    client.Reboot("recovery", exist);
+                    WriteLine("Done... \n");
                     break;
                 case 2:
                     {
@@ -57,7 +54,7 @@ public class Program
                    break;
                 case 5:                  
                     WriteLine("=====================================================================");
-                    WriteLine("| No.|    Name\t|     Model   \t |    State\t|     Serial\t  |");
+                    WriteLine("List of Devices attached");
                     foreach (var device in devices)
                     {
                         WriteLine($"  {devices.IndexOf(device)+1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
@@ -66,19 +63,20 @@ public class Program
                     break;
                 case 4:
                     WriteLine("=====================================================================");
-                    WriteLine("| No.|    Name\t|     Model   \t |    State\t|     Serial\t  |");
+                    WriteLine("List of Devices attached");
                     foreach (var device in devices)
                     {
                         WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
                     }
                     WriteLine("_____________________________________________________________________\n");
                     Write("Choose Device: ");
-                    var exist = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(ReadLine()));
-                    if(exist==null)
+                    var exist1 = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(ReadLine()));
+                    if(exist1==null)
                     {
                         WriteLine("Invalid entry.");
                     }
-                    client.Reboot("system", exist);
+                    client.Reboot("system", exist1);
+                    WriteLine("Done... \n");
                     break;
             }
         } while (choice!=0);
