8d71058ac725174593537f097a5977b9a6a9acc9
Wisdom Jere <wisdomjere5@gmail.com>
2022-02-26 05:30:22 +0200

    view devices

--- TWRPBackupTool/Program.cs
+++ TWRPBackupTool/Program.cs
@@ -1,12 +1,13 @@
 ﻿using static System.Console;
 using static System.Convert;
+using SharpAdbClient;
 using System.Diagnostics;
 namespace TWRPBackupTool;
 public class Program
 {
     static void DisplayMenu ()
     {
-        WriteLine("\t[1] Reboot to recovery\n\t[2] Backup\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[0] Quit\n");
+        WriteLine("\t[1] Reboot to recovery\n\t[2] Backup\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
     }
     static Task RebootToRecovery()
     {
@@ -23,15 +24,12 @@ public class Program
         WriteLine("Done.");
         return Task.CompletedTask;
     }
-    static Task Backup()
-    {
-        //check if the device is in recovery mode
-
-        return Task.CompletedTask;
-    }
+   
     public static async Task Main()
     {
         var choice = 0;
+        var server = new AdbServer();
+        var client = new AdbClient();
         do
         {
            DisplayMenu();
@@ -45,10 +43,29 @@ public class Program
                     choice = ToInt32(ReadLine());
                     break;
                 case 2:
-                    await Backup();
+                    {
+
+                        break;
+                    }
+                case 6:
+                   var result =server.StartServer(@"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe", false);
+        
+                   WriteLine("adb server: " + result.ToString());
+                   Write("Next Command: ");
+                   choice = ToInt32(ReadLine());                    
+                   break;
+                case 5:
+
+                    var devices = client.GetDevices();
+                    WriteLine("=====================================================================");
+                    WriteLine("| No.|    Name\t|     Model   \t |    State\t|     Serial\t  |");
+                    foreach (var device in devices)
+                    {
+                        WriteLine($"  {devices.IndexOf(device)+1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
+                    }
+                    WriteLine("_____________________________________________________________________\n");
                     break;
-                
-           }
+            }
         } while (choice!=0);
     }
 }
