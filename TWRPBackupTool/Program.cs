35cb71b922107cb15852765c394fce25e9f71d5b
Wisdom Jere <wisdomjere5@gmail.com>
2022-02-26 07:39:09 +0200

    restore implemented

--- TWRPBackupTool/Program.cs
+++ TWRPBackupTool/Program.cs
@@ -8,28 +8,28 @@ using System.Text;
 namespace TWRPBackupTool;
 public class Program
 {
-    
-    static void DisplayMenu ()
+
+    static void DisplayMenu()
     {
         WriteLine("\t[1] Reboot to recovery\n\t[2] Backup (disconnect other devices!!)\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
     }
-    
+
     public static async Task Main()
     {
         var choice = 0;
         var server = new AdbServer();
         var client = new AdbClient();
         var devices = client.GetDevices();
-       
-       
+
+
 
         do
         {
-           DisplayMenu();
-           Write("Reply: ");
-           choice = ToInt32(ReadLine());
-           switch(choice)
-           {
+            DisplayMenu();
+            Write("Reply: ");
+            choice = ToInt32(ReadLine());
+            switch (choice)
+            {
                 case 1:
                     WriteLine("=====================================================================");
                     WriteLine("List of Devices attached");
@@ -51,33 +51,24 @@ public class Program
                     {
                         //check if device is in recovery
 
-                        if(!devices.Any())
+                        if (!devices.Any())
                         {
                             WriteLine("No devices detected..");
                             break;
                         }
                         var dev = devices.First();
-                        if(dev.State !=DeviceState.Recovery)
+
+                        if (dev.State == DeviceState.Offline && dev.State == DeviceState.Unknown)
                         {
-                            WriteLine("Device is not in recovery mode, trying reboot..");
-                            if (dev.State == DeviceState.Offline && dev.State == DeviceState.Unknown)
-                            {
-                                WriteLine("Device is offline or unknown state");
-                                break;
-                            }
-                            else if(dev.State==DeviceState.Unauthorized)
-                            {
-                                WriteLine("Please enable usb debugging..");
-                                break;
-                            }
-                            else
-                            {
-                                client.Reboot("recovery", dev);
-                                WriteLine("Press any key to continue");
-                                ReadLine();
-                            }
+                            WriteLine("Device is offline or unknown state");
+                            break;
                         }
-                        var receivedMessage = new ConsoleOutputReceiver();
+                        else if (dev.State == DeviceState.Unauthorized)
+                        {
+                            WriteLine("Please enable usb debugging..");
+                            break;
+                        }                  
+         
                         var command = @"adb backup --twrp";
                         var startInfo = new ProcessStartInfo
                         {
@@ -91,26 +82,75 @@ public class Program
                         var output = new StringBuilder();
                         cmd.OutputDataReceived += (sender, args) => output.AppendLine(args.Data);
                         cmd.BeginOutputReadLine();
-                        WriteLine("Check your device and Select partitions to backup. Don't close this window until backup is finished."); 
+                        WriteLine("Check your device and Select partitions to backup. Don't close this window until backup is finished.");
                         cmd.WaitForExit();
                         string stdOut = output.ToString();
                         WriteLine(stdOut);
                         break;
                     }
                 case 3:
+                    {
+                        Write("Enter backup name (e.g) backup.ab");
+                        var filename = ReadLine();
+                        //check if device is in recovery
 
-                    break;
+                        if (!devices.Any())
+                        {
+                            WriteLine("No devices detected..");
+                            break;
+                        }
+                        var dev = devices.First();
+                        if (dev.State != DeviceState.Recovery)
+                        {
+                            WriteLine("Device is not in recovery mode, trying reboot..");
+                            if (dev.State == DeviceState.Offline && dev.State == DeviceState.Unknown)
+                            {
+                                WriteLine("Device is offline or unknown state");
+                                break;
+                            }
+                            else if (dev.State == DeviceState.Unauthorized)
+                            {
+                                WriteLine("Please enable usb debugging..");
+                                break;
+                            }
+                            else
+                            {
+                                client.Reboot("recovery", dev);
+                                WriteLine("Press any key to continue");
+                                ReadLine();
+                            }
+                        }
+                        var receivedMessage = new ConsoleOutputReceiver();
+                        var command = @$"adb restore {filename}";
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = "cmd.exe",
+                            Arguments = "/K " + command,
+                            RedirectStandardOutput = true,
+                            CreateNoWindow = true,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+                        using var cmd = Process.Start(startInfo);
+                        var output = new StringBuilder();
+                        cmd.OutputDataReceived += (sender, args) => output.AppendLine(args.Data);
+                        cmd.BeginOutputReadLine();
+                        WriteLine("Restore started don't close  this window until your phone reboots..");
+                        cmd.WaitForExit();
+                        string stdOut = output.ToString();
+                        WriteLine(stdOut);
+                        break;
+                    }
                 case 6:
-                   var result =server.StartServer(@"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe", false);
-        
-                   WriteLine("adb server: " + result.ToString());                                   
-                   break;
-                case 5:                  
+                    var result = server.StartServer(@"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe", false);
+
+                    WriteLine("adb server: " + result.ToString());
+                    break;
+                case 5:
                     WriteLine("=====================================================================");
                     WriteLine("List of Devices attached");
                     foreach (var device in devices)
                     {
-                        WriteLine($"  {devices.IndexOf(device)+1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
+                        WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
                     }
                     WriteLine("_____________________________________________________________________\n");
                     break;
@@ -124,7 +164,7 @@ public class Program
                     WriteLine("_____________________________________________________________________\n");
                     Write("Choose Device: ");
                     var exist1 = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(ReadLine()));
-                    if(exist1==null)
+                    if (exist1 == null)
                     {
                         WriteLine("Invalid entry.");
                     }
@@ -132,8 +172,8 @@ public class Program
                     WriteLine("Done... \n");
                     break;
             }
-        } while (choice!=0);
+        } while (choice != 0);
     }
-    
+
 }
 
