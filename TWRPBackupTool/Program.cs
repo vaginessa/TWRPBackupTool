bf082c7217dea981b017ea3cb0ac6fa1d2860204
Wisdom Jere <wisdomjere5@gmail.com>
2022-02-26 07:09:49 +0200

    try backup

--- TWRPBackupTool/Program.cs
+++ TWRPBackupTool/Program.cs
@@ -2,20 +2,26 @@
 using static System.Convert;
 using SharpAdbClient;
 using System.Diagnostics;
+using System.Net;
+
 namespace TWRPBackupTool;
 public class Program
 {
+    
     static void DisplayMenu ()
     {
-        WriteLine("\t[1] Reboot to recovery\n\t[2] Backup\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
-    } 
-   
+        WriteLine("\t[1] Reboot to recovery\n\t[2] Backup (disconnect other devices!!)\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
+    }
+    
     public static async Task Main()
     {
         var choice = 0;
         var server = new AdbServer();
         var client = new AdbClient();
         var devices = client.GetDevices();
+       
+       
+
         do
         {
            DisplayMenu();
@@ -42,15 +48,49 @@ public class Program
                     break;
                 case 2:
                     {
-
+                        //check if device is in recovery
+                        var dev = devices.First();
+                        if(dev.State !=DeviceState.Recovery)
+                        {
+                            WriteLine("Device is not in recovery mode, trying reboot..");
+                            if (dev.State == DeviceState.Offline && dev.State == DeviceState.Unknown)
+                            {
+                                WriteLine("Device is offline or unknown state");
+                                break;
+                            }
+                            else if(dev.State==DeviceState.Unauthorized)
+                            {
+                                WriteLine("Please enable usb debugging..");
+                                break;
+                            }
+                            else
+                            {
+                                client.Reboot("recovery", dev);
+                                WriteLine("Press any key to continue");
+                                ReadLine();
+                            }
+                        }
+                        var receivedMessage = new ConsoleOutputReceiver();
+                        var command = @"adb backup --twrp";
+                        var startInfo = new ProcessStartInfo
+                        {
+                            FileName = "cmd.exe",
+                            Arguments = "/K " + command,
+                            RedirectStandardOutput = true,
+                            CreateNoWindow = true,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+                        using var cmd = Process.Start(startInfo);
+                        string output = cmd.StandardOutput.ReadToEnd(); 
+                        WriteLine(output);
+                        cmd.WaitForExit();
+                        
                         break;
                     }
                 case 6:
                    var result =server.StartServer(@"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe", false);
         
-                   WriteLine("adb server: " + result.ToString());
-                   Write("Next Command: ");
-                   choice = ToInt32(ReadLine());                    
+                   WriteLine("adb server: " + result.ToString());                                   
                    break;
                 case 5:                  
                     WriteLine("=====================================================================");
