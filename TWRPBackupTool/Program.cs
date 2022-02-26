fe1049b1cea04da993c34006af53929270e8adce
Wisdom Jere <wisdomjere5@gmail.com>
2022-02-26 08:24:56 +0200

    added welcome text

--- TWRPBackupTool/Program.cs
+++ TWRPBackupTool/Program.cs
@@ -1,9 +1,11 @@
-﻿using static System.Console;
+﻿using Console = Colorful.Console;
 using static System.Convert;
 using SharpAdbClient;
 using System.Diagnostics;
 using System.Net;
 using System.Text;
+using Colorful;
+using System.Drawing;
 
 namespace TWRPBackupTool;
 public class Program
@@ -11,70 +13,74 @@ public class Program
     
     static void DisplayMenu()
     {
-        WriteLine("");
-        WriteLine("\t[1] Reboot to recovery\n\t[2] Backup (disconnect other devices!!)\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
+        
+        Console.WriteLine("\t[1] Reboot to recovery\n\t[2] Backup (disconnect other devices!!)\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[5] Check connectivity\n\t[6] Start Server\n\t[0] Quit\n");
     }
-
+    static void Welcome()
+    {
+        Console.WriteAscii("TWRP Backup Tool", Color.FromArgb(131, 184, 214));
+        Console.WriteLine("\t\t\t\t\t\t\t\tBy Wisdom Jere, github: @izzyjere", Color.Green);
+    }
     public static async Task Main()
     {
         var choice = 0;
         var server = new AdbServer();
         var client = new AdbClient();
         var devices = client.GetDevices();
-
+        Welcome();
 
 
         do
         {
             DisplayMenu();
-            Write("Reply: ");
-            choice = ToInt32(ReadLine());
+            Console.Write("Reply: ");
+            choice = ToInt32(Console.ReadLine());
             switch (choice)
             {
                 case 1:
-                    WriteLine("=====================================================================");
-                    WriteLine("List of Devices attached");
+                    Console.WriteLine("=====================================================================");
+                    Console.WriteLine("List of Devices attached");
                     foreach (var device in devices)
                     {
-                        WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
+                        Console.WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
                     }
-                    WriteLine("_____________________________________________________________________\n");
-                    Write("Choose Device: ");
-                    var exist = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(ReadLine()));
+                    Console.WriteLine("_____________________________________________________________________\n");
+                    Console.Write("Choose Device: ");
+                    var exist = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(Console.ReadLine()));
                     if (exist == null)
                     {
-                        WriteLine("Invalid entry.");
+                        Console.WriteLine("Invalid entry.");
                     }
                     client.Reboot("recovery", exist);
-                    WriteLine("Done... \n");
+                    Console.WriteLine("Done... \n");
                     break;
                 case 2:
                     {
                         //check if device is in recovery
                         if (!devices.Any())
                         {
-                            WriteLine("No devices detected..");
+                            Console.WriteLine("No devices detected..");
                             break;
                         }
                         var dev = devices.First();
                         if (dev.State != DeviceState.Recovery)
                         {
-                            WriteLine("Device is not in recovery mode, trying reboot..");
+                            Console.WriteLine("Device is not in recovery mode, trying reboot..");
                             if (dev.State == DeviceState.Offline && dev.State == DeviceState.Unknown)
                             {
-                                WriteLine("Device is offline or unknown state");
+                                Console.WriteLine("Device is offline or unknown state");
                                 break;
                             }
                             else if (dev.State == DeviceState.Unauthorized)
                             {
-                                WriteLine("Please enable usb debugging..");
+                                Console.WriteLine("Please enable usb debugging..");
                                 break;
                             }
                             else
                             {
                                 client.Reboot("recovery", dev);
-                                WriteLine("Press any key to continue");
-                                ReadLine();
+                                Console.WriteLine("Press any key to continue");
+                                Console.ReadLine();
                             }
                         }
                         var command = @"adb backup --twrp";
@@ -90,33 +96,33 @@ public class Program
                         var output = new StringBuilder();
                         cmd.OutputDataReceived += (sender, args) => output.AppendLine(args.Data);
                         cmd.BeginOutputReadLine();
-                        WriteLine("Check your device and Select partitions to backup. Don't close this window until backup is finished.");
+                        Console.WriteLine("Check your device and Select partitions to backup. Don't close this window until backup is finished.");
                         ShowSpinner();
                         cmd.WaitForExit();
                         string stdOut = output.ToString();
-                        WriteLine(stdOut);
+                        Console.WriteLine(stdOut);
                         break;
                     }
                 case 3:
                     {
-                        Write("Enter backup name (e.g) backup.ab");
-                        var filename = ReadLine();
+                        Console.Write("Enter backup name (e.g) backup.ab");
+                        var filename = Console.ReadLine();
                         //check if device is in recovery
                         if (!devices.Any())
                         {
-                            WriteLine("No devices detected..");
+                            Console.WriteLine("No devices detected..");
                             break;
                         }
                         var dev = devices.First();
 
                         if (dev.State == DeviceState.Offline && dev.State == DeviceState.Unknown)
                         {
-                            WriteLine("Device is offline or unknown state");
+                            Console.WriteLine("Device is offline or unknown state");
                             break;
                         }
                         else if (dev.State == DeviceState.Unauthorized)
                         {
-                            WriteLine("Please enable usb debugging..");
+                            Console.WriteLine("Please enable usb debugging..");
                             break;
                         }
                         var receivedMessage = new ConsoleOutputReceiver();
@@ -133,42 +139,49 @@ public class Program
                         var output = new StringBuilder();
                         cmd.OutputDataReceived += (sender, args) => output.AppendLine(args.Data);
                         cmd.BeginOutputReadLine();
-                        WriteLine("Restore started don't close  this window until your phone reboots..");
+                        Console.WriteLine("Restore started don't close  this window until your phone reboots..");
                         cmd.WaitForExit();
                         string stdOut = output.ToString();
-                        WriteLine(stdOut);
+                        Console.WriteLine(stdOut);
                         break;
                     }
                 case 6:
                     var result = server.StartServer(@"C:\Program Files (x86)\Android\android-sdk\platform-tools\adb.exe", false);
 
-                    WriteLine("adb server: " + result.ToString());
+                    Console.WriteLine("adb server: " + result.ToString());
                     break;
                 case 5:
-                    WriteLine("=====================================================================");
-                    WriteLine("List of Devices attached");
+                    Console.WriteLine("=====================================================================");
+                    Console.WriteLine("List of Devices attached");
                     foreach (var device in devices)
                     {
-                        WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
+                        Console.WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
                     }
-                    WriteLine("_____________________________________________________________________\n");
+                    Console.WriteLine("_____________________________________________________________________\n");
                     break;
                 case 4:
-                    WriteLine("=====================================================================");
-                    WriteLine("List of Devices attached");
+                    Console.WriteLine("=====================================================================");
+                    Console.WriteLine("List of Devices attached");
                     foreach (var device in devices)
                     {
-                        WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
+                        Console.WriteLine($"  {devices.IndexOf(device) + 1} \t{device.Name}   \t{device.Model}\t    {device.State}\t  {device.Serial}\n\n");
                     }
-                    WriteLine("_____________________________________________________________________\n");
-                    Write("Choose Device: ");
-                    var exist1 = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(ReadLine()));
+                    Console.WriteLine("_____________________________________________________________________\n");
+                    Console.Write("Choose Device: ");
+                    var exist1 = devices.FirstOrDefault(d => devices.IndexOf(d) + 1 == ToInt32(Console.ReadLine()));
                     if (exist1 == null)
                     {
-                        WriteLine("Invalid entry.");
+                        Console.WriteLine("Invalid entry.");
                     }
                     client.Reboot("system", exist1);
-                    WriteLine("Done... \n");
+                    Console.WriteLine("Done... \n");
+                    break;
+                default:
+                    Console.WriteLine("Invalid Option");
+                    break;
+                case 0:
+                    Console.Clear();
+                    Console.WriteAscii("Good Bye", Color.FromArgb(131, 184, 214));
                     break;
             }
         } while (choice != 0);
@@ -180,12 +193,12 @@ public class Program
         {
             switch (counter % 4)
             {
-                case 0: Write("/"); await Task.Delay(100); break;
-                case 1: Write("-"); await Task.Delay(100); break;
-                case 2: Write("\\"); await Task.Delay(100); break;
-                case 3: Write("|"); await Task.Delay(100); break;
+                case 0: Console.Write("/"); await Task.Delay(100); break;
+                case 1: Console.Write("-"); await Task.Delay(100); break;
+                case 2: Console.Write("\\"); await Task.Delay(100); break;
+                case 3: Console.Write("|"); await Task.Delay(100); break;
             }
-            SetCursorPosition(CursorLeft - 1, CursorTop);
+            Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
             counter++;
            
         }
