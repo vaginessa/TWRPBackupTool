96530f730a19d900cf81874d5a28e205757c4c7d
Wisdom Jere <wisdomjere5@gmail.com>
2022-02-26 04:03:13 +0200

    Add project files.

---
+++ TWRPBackupTool/Program.cs
@@ -0,0 +1,14 @@
+﻿using static System.Console;
+namespace TWRPBackupTool;
+public class Program
+{
+    static void DisplayMenu ()
+    {
+        WriteLine("\t[1] Reboot to recovery\n\t[2] Backup\n\t[3] Restore Backup\n\t[4] Reboot System\n\t[Q] Quit\n");
+    }
+    public static async Task Main()
+    {
+        DisplayMenu();
+    }
+}
+
